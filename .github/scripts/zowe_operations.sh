821cd860576b6d6b60bfffc9a976b6e10608ebcb
msengkuan <mak.sengkuan@dxc.com>
2026-03-08 16:59:36 +0800

    error  fix1"

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 # zowe_operations.sh
 
-# 1. INITIALIZE CONFIGURATION
-# Simply run 'init' without the force flag
+# 1. INITIALIZE CONFIG
 zowe config init
 
 # 2. SET CONNECTION PROPERTIES
-# Targeting standard 'base' and 'zosmf' profiles
-zowe config set "profiles.base.properties.host" "204.90.115.200"
-zowe config set "profiles.base.properties.user" "$ZOWE_USERNAME"
-zowe config set "profiles.base.properties.password" "$ZOWE_PASSWORD"
-zowe config set "profiles.base.properties.rejectUnauthorized" false
-zowe config set "profiles.zosmf.properties.port" 10443
+# We explicitly point to the config file just in case
+CONFIG_PATH="./zowe.config.json"
+
+zowe config set "profiles.base.properties.host" "204.90.115.200" --config-file "$CONFIG_PATH"
+zowe config set "profiles.base.properties.user" "$ZOWE_USERNAME" --config-file "$CONFIG_PATH"
+zowe config set "profiles.base.properties.password" "$ZOWE_PASSWORD" --config-file "$CONFIG_PATH"
+zowe config set "profiles.base.properties.rejectUnauthorized" false --config-file "$CONFIG_PATH"
+zowe config set "profiles.zosmf.properties.port" 10443 --config-file "$CONFIG_PATH"
 
 # 3. Setup Variables
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 TARGET_DIR="//z/$LOWERCASE_USERNAME/cobolcheck"
 
 # 4. Check/Create Directory
-# Using '//' to bypass Windows/Git Bash path translation
-if ! zowe zos-files list uss-files "$TARGET_DIR" &>/dev/null; then
+# We now use '--config-file' to force Zowe to use the values we just set
+if ! zowe zos-files list uss-files "$TARGET_DIR" --config-file "$CONFIG_PATH" &>/dev/null; then
     echo "Directory does not exist. Creating it..."
-    zowe zos-files create uss-directory "$TARGET_DIR"
+    zowe zos-files create uss-directory "$TARGET_DIR" --config-file "$CONFIG_PATH"
 else
     echo "Directory already exists. Skipping mkdir."
 fi
 
 # 5. Upload files
-# Using the standard dir-to-uss command
 zowe zos-files upload dir-to-uss "." "$TARGET_DIR" \
   --recursive \
-  --binary-files "cobol-check-0.2.19.jar"
+  --binary-files "cobol-check-0.2.19.jar" \
+  --config-file "$CONFIG_PATH"
